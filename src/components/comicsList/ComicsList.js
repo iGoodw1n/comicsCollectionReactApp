944471254f1065a15053d689c717d0ba1934546e
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2024-01-11 19:44:05 +0400

    Initial

---
+++ src/components/comicsList/ComicsList.js
@@ -0,0 +1,85 @@
+import './comicsList.scss';
+import { useEffect, useRef, useState } from 'react';
+import useMarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Spinner from '../spinner/Spinner';
+import { Link } from 'react-router-dom';
+
+const ComicsList = () => {
+    const [comicsList, setComicsList] = useState([]);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(0);
+    const [comicsEnded, setComicsEnded] = useState(false);
+    const effectCalled = useRef(false);
+
+    const { loading, error, getAllComics } = useMarvelService();
+
+    useEffect(() => {
+        if (effectCalled.current) return;
+        onRequest(offset, true);
+        effectCalled.current = true;
+    }, [])
+
+    const onRequest = (offset, initial) => {
+        setNewItemLoading(initial ? false : true)
+        getAllComics(offset)
+            .then(onComicsListLoaded)
+    }
+
+    const onComicsListLoaded = (newComicsList) => {
+        let ended = false;
+        if (newComicsList.length < 8) {
+            ended = true;
+        }
+
+        setComicsList(comicsList => [...comicsList, ...newComicsList]);
+        setNewItemLoading(false);
+        setOffset(offset => offset + 8);
+        setComicsEnded(ended);
+    }
+
+    function renderItems(arr) {
+        const items = arr.map((item, i) => {
+            let imgStyle = { 'objectFit': 'cover' };
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = { 'objectFit': 'unset' };
+            }
+
+            return (
+                <li key={i} className="comics__item">
+                    <Link to={`/comics/${item.id}`}>
+                        <img src={item.thumbnail} alt={item.title} className="comics__item-img" style={imgStyle}/>
+                        <div className="comics__item-name">{item.title}</div>
+                        <div className="comics__item-price">{item.price}$</div>
+                    </Link>
+                </li>
+            )
+        });
+        return (
+            <ul className="comics__grid">
+                {items}
+            </ul>
+        )
+    }
+
+    const items = renderItems(comicsList);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading && !newItemLoading ? <Spinner /> : null;
+    return (
+        <div className="comics__list">
+            {errorMessage}
+            {spinner}
+            {items}
+            <button
+                className="button button__main button__long"
+                disabled={newItemLoading}
+                style={{ display: comicsEnded ? 'none' : 'block' }}
+                onClick={() => onRequest(offset)}>
+                <div className="inner">load more</div>
+            </button>
+        </div>
+    )
+}
+
+export default ComicsList;
